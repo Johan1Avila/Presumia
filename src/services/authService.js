37d37d213c64db01f366416191c11f342fd41897
Johan1Avila <johanavilagit@gmail.com>
2025-08-20 17:11:32 -0500

    feat: crear servicios de auth (register, login, logout)

---
+++ src/services/authService.js
@@ -0,0 +1,47 @@
+// services/authService.js
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+import { auth } from '../firebase/firebaseAuth';
+
+// registrar un usuario
+export async function registerUser(email, password) {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    );
+    return userCredential.user; // devuelve el usuario creado
+  } catch (error) {
+    throw new Error(error.message); // lanza el error para manejarlo en el componente
+  }
+}
+
+// Iniciar sesión
+export async function loginUser(email, password) {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    );
+    return userCredential.user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Cerrar sesión
+export const logoutUser = async () => {
+  try {
+    await signOut(auth);
+    console.log('Sesión cerrada ✅');
+    return true;
+  } catch (error) {
+    console.error('Error al cerrar sesión:', error);
+    throw error;
+  }
+};
